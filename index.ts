685f0cdbe17ef9f1be185d316d338c3317bba609
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-05-08 22:29:56 -0700

    prepare for heroku

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const port = parseInt((process.env.PORT as string) ?? 8080);
+const wss = new WebSocket.Server({ port });
 
-console.log("Server started");
+console.log(`Server started on port ${port}`);
 // i know this won't scale well with lots of rooms
 const rooms: any = [];
 
 wss.on("connection", (ws) => {
-  console.log(ws);
   ws.on("message", (message) => {
     const recievedJSON = JSON.parse(message.toString());
     const { action, payload } = recievedJSON;
@@ -24,8 +24,30 @@ wss.on("connection", (ws) => {
         const roomIndex = rooms.findIndex(
           (room) => room.roomId === payload.roomId
         );
-        rooms[roomIndex].users.push(ws);
-        console.log(rooms);
+        rooms[roomIndex].users.push({
+          username: payload.username,
+          ws,
+        });
+        ws.send(
+          JSON.stringify({
+            action: "message",
+            message: `Welcome to room ${payload.roomId} with ${rooms[roomIndex].users.length} people`,
+          })
+        );
+        break;
+      }
+      case "playbackToggle": {
+        const roomIndex = rooms.findIndex(
+          (room) => room.roomId === payload.roomId
+        );
+        rooms[roomIndex].users.forEach((user) => {
+          user.ws.send(
+            JSON.stringify({
+              action: "playbackToggle",
+              message: `${payload.username} toggled playback`,
+            })
+          );
+        });
         break;
       }
       default: {
